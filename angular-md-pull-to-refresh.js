780b05f78eedda6456abce9737be6d76d222f3d2
Will Chiong <will@infomofo.com>
2015-02-11 09:21:59 -0500

    make stricter

--- angular-md-pull-to-refresh.js
+++ angular-md-pull-to-refresh.js
@@ -20,6 +20,7 @@ angular.module('angularMdPullToRefresh', [])
         scope.hasCallback = angular.isDefined(attrs.refreshFunction);
         scope.isAtTop = false;
         scope.pullToRefreshActive = false;
+        scope.lastScrollTop = 0;
 
         scope.pullToRefresh = function () {
           if (scope.hasCallback) {
@@ -37,7 +38,7 @@ angular.module('angularMdPullToRefresh', [])
         // Wait 1 second and then add an event listener to the scroll events on this list- this enables pull to refresh functionality
         $timeout(function () {
           var onScroll = function () {
-            if (element[0].scrollTop <= 1) {
+            if (element[0].scrollTop <= 0) {
               if (scope.isAtTop) {
                 scope.pullToRefresh();
               } else {
