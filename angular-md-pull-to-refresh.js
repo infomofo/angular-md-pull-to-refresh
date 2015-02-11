4aa5071355ef7b0d7d2115138352540b1c392628
Will Chiong <will@infomofo.com>
2015-02-11 09:36:13 -0500

    pull to refresh

--- angular-md-pull-to-refresh.js
+++ angular-md-pull-to-refresh.js
@@ -7,52 +7,53 @@
  * # pullToRefresh
  */
 angular.module('angularMdPullToRefresh', [])
-  .directive('pullToRefresh', function ($timeout) {
-    return {
-      restrict: 'A',
-      transclude: true,
-      template: '<md-progress-linear md-mode="indeterminate" class="md-accent ng-hide" ng-show="pullToRefreshActive"></md-progress-linear><ng-transclude></ng-transclude>',
-      scope: {
-        refreshFunction: '&' // This function is expected to return a future
-      },
-      link: function postLink(scope, element, attrs) {
+    .directive('pullToRefresh', function ($timeout) {
+      return {
+        restrict: 'A',
+        transclude: true,
+        template: '<md-progress-linear md-mode="indeterminate" class="md-accent ng-hide" ng-show="pullToRefreshActive"></md-progress-linear><ng-transclude></ng-transclude>',
+        scope: {
+          refreshFunction: '&' // This function is expected to return a future
+        },
+        link: function postLink(scope, element, attrs) {
 
-        scope.hasCallback = angular.isDefined(attrs.refreshFunction);
-        scope.isAtTop = false;
-        scope.pullToRefreshActive = false;
-        scope.lastScrollTop = 0;
+          scope.hasCallback = angular.isDefined(attrs.refreshFunction);
+          scope.isAtTop = false;
+          scope.pullToRefreshActive = false;
+          scope.lastScrollTop = 0;
 
-        scope.pullToRefresh = function () {
-          if (scope.hasCallback) {
-            if (!scope.pullToRefreshActive) {
-              scope.pullToRefreshActive = true;
-              scope.refreshFunction().then(function () {
-                scope.pullToRefreshActive = false;
-              });
-              scope.$digest();
-            }
-          }
-
-        };
-
-        // Wait 1 second and then add an event listener to the scroll events on this list- this enables pull to refresh functionality
-        $timeout(function () {
-          var onScroll = function () {
-            if (element[0].scrollTop <= 0) {
-              if (scope.isAtTop) {
-                scope.pullToRefresh();
-              } else {
-                scope.isAtTop = true;
+          scope.pullToRefresh = function () {
+            if (scope.hasCallback) {
+              if (!scope.pullToRefreshActive) {
+                scope.pullToRefreshActive = true;
+                scope.refreshFunction().then(function () {
+                  scope.pullToRefreshActive = false;
+                });
+                scope.$digest();
               }
             }
+
           };
-          element[0].addEventListener('scroll', onScroll);
-          element[0].addEventListener('touchmove', onScroll);
-        }, 1000);
+
+          // Wait 1 second and then add an event listener to the scroll events on this list- this enables pull to refresh functionality
+          $timeout(function () {
+            var onScroll = function (event) {
+              if (element[0].scrollTop <= 0 && scope.lastScrollTop <= 0) {
+                if (scope.isAtTop) {
+                  scope.pullToRefresh();
+                } else {
+                  scope.isAtTop = true;
+                }
+              }
+              scope.lastScrollTop = element[0].scrollTop;
+            };
+            element[0].addEventListener('scroll', onScroll);
+            element[0].addEventListener('touchmove', onScroll);
+          }, 1000);
 
 
 
 
-      }
-    };
-  });
+        }
+      };
+    });
